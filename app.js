6201f6482d6841ec7cd3f246a0871f373b29561a
Miguel Gaviota <mgavitogzz@gmail.com>
2024-09-16 14:24:27 -0600

    añadi el diccionario de fonemas de ingles y español y cambie un poco el style

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Proyecto iniciando"); 
+const fonemasIngles = [
+    'iː', 'ɪ', 'e', 'æ', 'ɑː', 'ɒ', 'ɔː', 'ʊ', 'uː', 'ʌ', 'ə',  // Vocales
+    'aɪ', 'eɪ', 'aʊ', 'ɔɪ', 'əʊ', 'oʊ',  // Diptongos
+    'p', 'b', 't', 'd', 'k', 'g', 'f', 'v', 'θ', 'ð', 
+    's', 'z', 'ʃ', 'ʒ', 'tʃ', 'dʒ', 'm', 'n', 'ŋ', 
+    'l', 'r', 'j', 'w', 'h'  // Consonantes
+];
+const fonemaEspanol = [
+    'a', 'e', 'i', 'o', 'u',  // Vocales
+    'p', 'b', 't', 'd', 'k', 'g', 'f', 's', 'x', 'ʝ', 'ʎ', 
+    'm', 'n', 'ɲ', 'r', 'ɾ', 'l', 'ʃ', 'tʃ', 'θ', 
+    'β', 'ð', 'ɣ'  // Consonantes
+  ];
