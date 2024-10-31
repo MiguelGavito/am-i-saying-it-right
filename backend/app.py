08acfe89248961949df63c5c711703fd9e7bc136
Miguel Gaviota <mgavitogzz@gmail.com>
2024-10-31 09:37:48 -0600

    La pagina se abre y puede devolver la fonetica de las palabras, pero da un formato distinto al AFI, lo habia visto antes pero no me gusta para visualizar esto, se requiere modificar el diseño de la pagina y conseguir colores propios y tipo de letra en bootstrap. Hay un error que provoca que la pagina se cierre si se introduce una palabra inexistente, en ese caso se pone toda la pagina en blanco, tengo que hacer un catch de este erro rpor si las dudas luego y averiguar como que el programa en caso de no encontrar la palabra busque una similar, eso es ya muy a futuro

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,31 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import pronouncing
 
 app = Flask(__name__)
 CORS(app)
 
 
+def get_transcription(word):
+    phones = pronouncing.phones_for_word(word)
+    if not phones:
+        return []
+
+    transcription = [{"character": char, "color": "blue"} for char in phones[0].split()]
+    return transcription
+
+
 @app.route("/api/transcription", methods=["POST"])
 def transcription():
     data = request.json
-    word = data.get("word")
+    word = data.get("word", "").lower()
+    transcription = get_transcription(word)
 
-    transcription = [
-        {"character": "h", "color": "green"},
-        {"character": "ə", "color": "yellow"},
-        {"character": "l", "color": "red"},
-        {"character": "oʊ", "color": "yellow"},
-    ]
+    if not transcription:
+        return jsonify({"Error": "Word not found"}), 404
 
     return jsonify({"transcription": transcription})
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     app.run(debug=True)
